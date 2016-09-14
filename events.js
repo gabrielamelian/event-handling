2274a6eea51250609a1484fde57d443f03170289
gabrielamelian <gabrielamelian@gmail.com>
2016-09-14 20:47:21 +1200

    Add four function

--- events.js
+++ events.js
@@ -9,6 +9,7 @@ function start () {
   one()
   two()
   three()
+  four()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -38,6 +39,11 @@ function three () {
 }
 
 // CREATE FUNCTION four HERE
+function four () {
+  var four = document.getElementById('four');
+  four.addEventListener('click', makeBlue);
+  four.addEventListener('mouseleave', makeWhite);
+}
 
 // Changes the background color of event's target
 function makeBlue (evt) {
